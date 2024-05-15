5a047d9a95dddd52a12fbe46e2ca6d01bf867ba2
lxndrbukin <lxndrbukin@gmail.com>
2024-05-15 07:41:43 +0100

    Refactor HeaderDropdownMenu background pop up with redux state instead of useState

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,29 @@
 import './assets/styles.scss';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import HeaderMenu from './HeaderMenu';
 import HeaderSearch from './HeaderSearch';
 import HeaderUserSections from './HeaderUserSections';
+import { RootState } from '../../store';
 
 export default function Header(): JSX.Element {
+  const { showMenuDropdown } = useSelector(
+    (state: RootState) => state.system.ui.header
+  );
+
+  const dropdownBackground = <div className="header-menu-dropdown-bg"></div>;
+
   return (
-    <header className='header-container'>
-      <div className='header'>
-        <Link to='/' className='header-logo'>
+    <header className="header-container">
+      <div className="header">
+        <Link to="/" className="header-logo">
           uniSTORE
         </Link>
         <HeaderMenu />
         <HeaderSearch />
         <HeaderUserSections />
       </div>
+      {showMenuDropdown && dropdownBackground}
     </header>
   );
 }
--- src/components/Header/HeaderMenu.tsx
+++ src/components/Header/HeaderMenu.tsx
@@ -1,36 +1,37 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState, showDropdown } from '../../store';
 import { BiChevronDown } from 'react-icons/bi';
 import HeaderMenuDropdown from './HeaderMenuDropdown';
 
 export default function HeaderMenu(): JSX.Element {
-  const [isVisible, setIsVisible] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const { showMenuDropdown } = useSelector(
+    (state: RootState) => state.system.ui.header
+  );
 
   const onMouseEvent = () => {
-    setIsVisible(!isVisible);
+    dispatch(showDropdown(!showMenuDropdown));
   };
 
-  const dropdownBackground = <div className='header-menu-dropdown-bg'></div>;
-
   return (
-    <nav className='header-menu'>
-      <ul className='header-menu-links'>
+    <nav className="header-menu">
+      <ul className="header-menu-links">
         <li
           onMouseEnter={onMouseEvent}
           onMouseLeave={onMouseEvent}
-          className='header-menu-link'
+          className="header-menu-link"
         >
           Categories <BiChevronDown size={19} />
-          {isVisible && <HeaderMenuDropdown />}
+          {showMenuDropdown && <HeaderMenuDropdown />}
         </li>
-        <li className='header-menu-link'>
-          <Link to='/new-products'>What's New</Link>
+        <li className="header-menu-link">
+          <Link to="/new-products">What's New</Link>
         </li>
-        <li className='header-menu-link'>
-          <Link to='/deals'>Deals</Link>
+        <li className="header-menu-link">
+          <Link to="/deals">Deals</Link>
         </li>
       </ul>
-      {isVisible && dropdownBackground}
     </nav>
   );
 }
